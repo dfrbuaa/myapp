12d2f6f1b90782db0f39f7bedace00ff12024cd2
dfrbuaa <1162964677@qq.com>
2022-02-06 18:33:13 +0800

    第一天

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App (props) {
+  let content;
+  if (props.vip === "true") {
+    content = <h1>VIP您好，欢迎回来</h1>
+  } else {
+    content = <h1>分享砍会员红包</h1>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 条件渲染 */}
+      {content}
+      {/* 通过三元运算符实现条件渲染 */}
+      {props.vip == "true" ? <h1>VIP等级s</h1> : <h1>充值领取VIP</h1>}
+      <Listcon></Listcon>
     </div>
   );
 }
 
+// 列表组件
+function Listcon (params) {
+  let students = ["xiaoming", "xiaohong", "xiaohei"]
+  let stars = [<li>魏晨</li>, <li>张云雷</li>, <li>meme</li>]
+  let studentsJsx = students.map((ele, i) => {
+    return <li key={i}>{ele}</li>
+  })
+  return (
+    <div>
+      {/* 数组渲染列表 */}
+      {/* <ul>
+        {students}
+      </ul>
+      <ul>
+        {stars}
+      </ul> */}
+      <ul>
+        {studentsJsx}
+      </ul>
+    </div>
+  )
+}
 export default App;
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// 展示网页整体性能
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
